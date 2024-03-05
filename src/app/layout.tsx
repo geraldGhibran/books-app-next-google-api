5fa5f33e70c1659ed983ea310e436cd4481cb7d0
geraldGhibran <gerald.gunasha@gmail.com>
2024-03-05 08:43:37 +0700

    Revert "Add fixing performance"
    This reverts commit a84993aa9c595cd72b2a9f8ffd7b400f2d2dfde2.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,22 +4,13 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'books app',
-  description:
-    'Created with next js and Google Books API by Gerald Ghibran : https://github.com/geraldGhibran/books-app-next-google-api'
+  title: 'books app'
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `</script><link rel='preload' href='style.css' as='style' onload="this.onload=null;this.rel='stylesheet'"/><script>`
-        }}
-      />
-    </>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
   );
 }
